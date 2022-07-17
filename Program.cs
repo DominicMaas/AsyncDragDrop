af9a8c6b09a4b1d9e466d155a842d6db9ed2063b
Dominic Maas <dominic.maas@live.com>
2022-07-18 09:00:54 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.UI.Dispatching;
+using Microsoft.Windows.AppLifecycle;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDragDrop;
+
+public static class Program
+{
+    [global::System.Runtime.InteropServices.DllImport("Microsoft.ui.xaml.dll")]
+    private static extern void XamlCheckProcessRequirements();
+
+    [STAThread]
+    public static async Task Main(string[] args)
+    {
+        XamlCheckProcessRequirements();
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+
+        var mainInstance = AppInstance.FindOrRegisterForKey("main");
+        if (!mainInstance.IsCurrent)
+        {
+            // Redirect the activation (and args) to the "main" instance, and exit.
+            var activatedEventArgs = AppInstance.GetCurrent().GetActivatedEventArgs();
+            await mainInstance.RedirectActivationToAsync(activatedEventArgs);
+
+            return;
+        }
+
+        Microsoft.UI.Xaml.Application.Start((p) =>
+        {
+            var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+            SynchronizationContext.SetSynchronizationContext(context);
+            new App();
+        });
+    }
+}
